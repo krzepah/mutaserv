2ae7b0341801e16a3ccf7e694a72b7a0c61cbcde
Patrick Borowy <patrick.borowy@gmail.com>
2019-07-28 02:50:24 +0200

    file imports use cwd

--- command.js
+++ command.js
@@ -7,14 +7,14 @@ const path = require('path');
 
 const prog = sade(pjson.name);
 
-const port = (pjson, dest) => dest.port ? dest.port : pjson.mutaserv.port ? pjson.mutaserv.port : 9000;
-const mutations = (pjson, dest) => {
+const port = (cpjson, dest) => dest.port ? dest.port : cpjson.port ? cpjson.port : 9000;
+const mutations = (cpjson, dest) => {
 	if (dest.mutations || dest.m)
-		return dest.mutations || dest.m;
-	else if (pjson.mutaserv.mutations)
-		return pjson.mutaserv.mutations;
+		return path.resolve(process.cwd(), dest.mutations || dest.m);
+	else if (cpjson.mutations)
+		return path.resolve(process.cwd(), cpjson.mutations);
 	process.stdout.write('WARNING : loaded example mutations\n');
-	return path.dirname(require.main.filename) + '/example.js';
+	return path.resolve(path.dirname(require.main.filename), './example.js');
 };
  
 prog
@@ -25,20 +25,15 @@ prog
 	.describe('Serves a mutation file.')
 	.option('-p, --port', 'Change the default port', undefined)
 	.option('-m, --muts', 'Change the default muts', undefined)
-	.option('-c, --config', 'Provide path to custom package.json', 'package.json')
+	.option('-c, --conf', 'Provide path to custom package.json', 'package.json')
 	.example('run')
 	.action((dest, opts) => {
-		process.stdout.write(
-			'Mutaserv ' +
-			pjson.version +
-			' starting on port ' +
-			port(pjson, dest) +
-			'\n'
-		);
+		const cpjson = require(path.resolve(process.cwd(), dest.conf)).mutaserv;
+		process.stdout.write('Mutaserv ' + pjson.version + ' starting on port ' + port(cpjson, dest) + '\n');
 		process.env.NODE_ENV = 'development';
-		process.env.MUTATIONS = mutations(pjson, dest);
+		process.env.MUTATIONS = mutations(cpjson, dest);
 		const app = require('./api');
-		app.listen(port(pjson, dest), err => {
+		app.listen(port(cpjson, dest), err => {
 			if (err) throw err;
 		});
 	});
