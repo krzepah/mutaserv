bf8d8c3b65a992a342960c88b175b551c95220e0
Patrick Borowy <patrick.borowy@gmail.com>
2019-07-31 17:29:31 +0200

    moved main loadEnv command to a seperate function

--- command.js
+++ command.js
@@ -23,6 +23,95 @@ const adbval = (config, opts, key, def, prefix) => opts[prefix + key] ?
 prog
 	.version(pjson.version);
 
+
+const loadEnv = (opts) => {
+	let config = { database: { }, log: { } };
+	if (!opts.skip && fs.existsSync(path.resolve(process.cwd(), opts.conf)))
+		config = require(path.resolve(process.cwd(), opts.conf)).mutaserv;
+	else if (!opts.skip)
+		config = require(process.env.MUTA_CONFIG);
+
+	if (opts['log.mutations'])
+		process.env.LOGS_MUTATIONS = opts['log.mutations'] !== true;
+	else if (config && config.log && config.log.mutations != undefined)
+		process.env.LOGS_MUTATIONS = config.log.mutations !== 'true' && !(config.log.mutations);
+	else
+		process.env.LOGS_MUTATIONS = false;
+
+	if (opts['log.nocolor'])
+		process.env.LOGS_COLOR = opts['log.nocolor'] !== true;
+	else if (config && config.log && config.log.nocolor != undefined)
+		process.env.LOGS_COLOR = config.log.nocolor !== 'true' && !(config.log.nocolor);
+	else
+		process.env.LOGS_COLOR = true;
+
+	if (opts['log.nostdout'])
+		process.env.LOGS_NOSTDOUT = opts['log.nostdout'];
+	else if (config && config.log && config.log.nostdout != undefined)
+		process.env.LOGS_NOSTDOUT = config.log.nostdout;
+	else
+		process.env.LOGS_NOSTDOUT = false;
+
+	if (opts['log.path'])
+		process.env.LOGS_PATH = path.resolve(process.cwd(), opts['log.path']);
+	else if (config && config.log && config.log.path)
+		process.env.LOGS_PATH = path.resolve(path.dirname(opts.c), config.log.path);
+	else
+		process.env.LOGS_PATH = path.resolve(process.cwd(), 'mutaserv.log');
+
+	const logger = require('./config/logger');
+	const port = (config, opts) => opts.port ?
+		opts.port
+		: config ?
+			config.port ?
+				config.port
+				: 9000
+			: 9000;
+	logger.info(
+		'Mutaserv ' + pjson.version + ' starting on port ' + port(config, opts)
+	);
+	logger.info('Logging into ' + process.env.LOGS_PATH);
+	if (!opts.skip && config === undefined)
+		logger.warn('No configuration found in ' + opts.conf);
+	else if (opts.skip)
+		logger.info('No configuration loaded');
+	else if (opts['log.config'])
+		logger.info('Loaded config : ' + JSON.stringify(config, null, 4));
+	if (opts['log.args'])
+		logger.info('Loaded with following args : ' + JSON.stringify(opts, null, 4));
+
+	const mutations = (config, opts) => {
+		if (opts.mutations || opts.m) {
+			return path.resolve(process.cwd(), opts.mutations || opts.m);
+		}
+		else if (config && config.mutations) {
+			return path.resolve(path.dirname(opts.c), config.mutations);
+		}
+		logger.warn('Loaded with test mutations (mutaserv run -h for help)');
+		return path.resolve(path.dirname(require.main.filename), './test/mutations.js');
+	};
+	process.env.MUTATIONS = mutations(config, opts);
+
+	ramda.map(([key, def]) => process.env['DB_' + key.toUpperCase()] = dbval(
+		config, opts, key, def, 'db.'
+	), [
+		['verbose', false],
+		['dialect', 'sqlite'],
+		['host', 'mutaserv.db'],
+		['username', 'mutaserv'],
+		['password', 'mutaserv']
+	]);
+	process.env.DB_PATH = adbval(config, opts, 'path', path.resolve(process.cwd(), 'mutaserv.db'), 'db.');
+	if (opts.e || opts['log.env']) {
+		// eslint-disable-next-line
+		logger.info('env : ' + JSON.stringify(process.env, null, 4));
+	}
+	const app = require('./api');
+	app.listen(port(config, opts), err => {
+		if (err) throw err;
+	});
+};
+
 prog
 	.command('run')
 	.describe('Serves a mutation file.')
@@ -44,90 +133,9 @@ prog
 	.option('--db.username', 'Database username	 (default mutaserv)')
 	.option('--db.password', 'Database password	 (default mutaserv)')
 	.example('run')
-	.action((opts) => {
-		let config = { database: { }, log: { } };
-		if (!opts.skip && fs.existsSync(path.resolve(process.cwd(), opts.conf)))
-			config = require(path.resolve(process.cwd(), opts.conf)).mutaserv;
-
-		if (opts['log.mutations'])
-			process.env.LOGS_MUTATIONS = opts['log.mutations'] !== true;
-		else if (config && config.log && config.log.mutations != undefined)
-			process.env.LOGS_MUTATIONS = config.log.mutations !== 'true' && !(config.log.mutations);
-		else
-			process.env.LOGS_MUTATIONS = false;
-
-		if (opts['log.nocolor'])
-			process.env.LOGS_COLOR = opts['log.nocolor'] !== true;
-		else if (config && config.log && config.log.nocolor != undefined)
-			process.env.LOGS_COLOR = config.log.nocolor !== 'true' && !(config.log.nocolor);
-		else
-			process.env.LOGS_COLOR = true;
-
-		if (opts['log.nostdout'])
-			process.env.LOGS_NOSTDOUT = opts['log.nostdout'];
-		else if (config && config.log && config.log.nostdout != undefined)
-			process.env.LOGS_NOSTDOUT = config.log.nostdout;
-		else
-			process.env.LOGS_NOSTDOUT = false;
-	
-		if (opts['log.path'])
-			process.env.LOGS_PATH = path.resolve(process.cwd(), opts['log.path']);
-		else if (config && config.log && config.log.path)
-			process.env.LOGS_PATH = path.resolve(path.dirname(opts.c), config.log.path);
-		else
-			process.env.LOGS_PATH = path.resolve(process.cwd(), 'mutaserv.log');
-	
-		const logger = require('./config/logger');
-		const port = (config, opts) => opts.port ?
-			opts.port
-			: config ?
-				config.port ?
-					config.port
-					: 9000
-				: 9000;
-		logger.info(
-			'Mutaserv ' + pjson.version + ' starting on port ' + port(config, opts)
-		);
-		logger.info('logging into ' + process.env.LOGS_PATH);
-		if (!opts.skip && config === undefined)
-			logger.warn('no configuration found in ' + opts.conf);
-		else if (opts.skip)
-			logger.info('no configuration loaded');
-		else if (opts['log.config'])
-			logger.info('loaded config : ' + JSON.stringify(config, null, 4));
-		if (opts['log.args'])
-			logger.info('loaded with following args : ' + JSON.stringify(opts, null, 4));
-
-		const mutations = (config, opts) => {
-			if (opts.mutations || opts.m) {
-				return path.resolve(process.cwd(), opts.mutations || opts.m);
-			}
-			else if (config && config.mutations) {
-				return path.resolve(path.dirname(opts.c), config.mutations);
-			}
-			logger.warn('loaded with example mutations (mutaserv run -h for help)');
-			return path.resolve(path.dirname(require.main.filename), './example.js');
-		};
-		process.env.MUTATIONS = mutations(config, opts);
-
-		ramda.map(([key, def]) => process.env['DB_' + key.toUpperCase()] = dbval(
-			config, opts, key, def, 'db.'
-		), [
-			['verbose', false],
-			['dialect', 'sqlite'],
-			['host', 'mutaserv.db'],
-			['username', 'mutaserv'],
-			['password', 'mutaserv']
-		]);
-		process.env.DB_PATH = adbval(config, opts, 'path', path.resolve(process.cwd(), 'mutaserv.db'), 'db.');
-		if (opts.e || opts['log.env']) {
-			// eslint-disable-next-line
-			logger.info('env : ' + JSON.stringify(process.env, null, 4));
-		}
-		const app = require('./api');
-		app.listen(port(config, opts), err => {
-			if (err) throw err;
-		});
-	});
-
+	.action((opts) => loadEnv(opts));
+		
+		
 prog.parse(process.argv);
+
+module.exports = loadEnv;
